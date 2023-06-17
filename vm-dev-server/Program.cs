f6c70ce02af1dbb4fc1c6a7c73c6c2af72d98950
vmangahis <just.monika092299@gmail.com>
2023-06-17 22:44:25 +0800

    configured for cloud migration

--- vm-dev-server/Program.cs
+++ vm-dev-server/Program.cs
@@ -1,5 +1,7 @@
 global using vm_dev_server.Data;
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using System;
 
 namespace vm_dev_server
 {
@@ -20,7 +22,8 @@ namespace vm_dev_server
             builder.Services.AddCors();
             builder.Services.AddDbContext<DataContext>(options =>
             {
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+                options.UseSqlServer(GetConnectionString(builder));//builder.Configuration.GetConnectionString("DefaultConnection"));
+                Environment.GetEnvironmentVariable("DATABASE_URL");
             });
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
@@ -57,6 +60,33 @@ namespace vm_dev_server
 
 
             app.Run();
+        }
+
+        public static string GetConnectionString(WebApplicationBuilder wb)
+        {
+            var dbURL = Environment.GetEnvironmentVariable("DATABASE_URL");
+            var conString = wb.Configuration.GetConnectionString("DefaultConnection");
+            return string.IsNullOrEmpty(dbURL) ? conString : BuildCS(dbURL);
+        }
+
+        private static string BuildCS(string dbURL) {
+            var dburl = new Uri(dbURL);
+            var userin = dburl.UserInfo.Split(':');
+            var builder = new NpgsqlConnectionStringBuilder
+            {
+                Host = dburl.Host,
+                Port = dburl.Port,
+                Username = userin[0],
+                Password = userin[1],
+                Database = dburl.LocalPath.TrimStart('/'),
+                SslMode = SslMode.Require,
+                TrustServerCertificate = true
+
+
+            };
+
+            return builder.ToString();
+        
         }
 
 
